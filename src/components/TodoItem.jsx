af7c9b3c2f8fa2b204c453644587045ec58db69d
abhijithpr08 <abhith628@gmail.com>
2025-12-29 12:23:38 +0530

    set item edit, delete

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,34 @@
 import { useState } from "react";
 import { useTodo } from "../context/TodoContext";
 
-const TodoItem = () =>{
+const TodoItem = ({todo}) =>{
+    const {deleteTodo,updateTodo}= useTodo;
+    const [edit,setEdit]=useState(false)
+    const [text,setText]=useState(todo.text)
 
+    return (
+        <li>
+            {edit ?(<input value={text} onChange={(e)=>setText(e.target.value)}/>)
+            :
+            (<span>{todo.text}</span>)}
+
+            <div>
+                {
+                    edit ? ( <button onClick={()=>{
+                    updateTodo(todo.id, text);
+                    setEdit(false);
+                }}>SAVE</button>)
+                :
+                (<button onClick={() => setEdit(true)}
+                    
+                    >EDIT</button>)
+                }
+                <button onClick={()=> deleteTodo(todo.id)}>
+                DELETE    
+                </button>
+            </div>
+        </li>
+    )
 }
 
 export default TodoItem
