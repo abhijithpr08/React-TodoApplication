6f7e40bb2910a87f3dd1d1bdb9977d47961ae281
abhijithpr08 <abhith628@gmail.com>
2025-12-29 11:51:38 +0530

    create form

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,8 +1,22 @@
 import { useTodo } from "../context/TodoContext";
 import { useState } from "react";
 
-const TodoForm = ()=>{
-    
+const TodoForm = () => {
+    const [text, setText] = useState("")
+    const { addTodo } = useTodo();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!text.trim()) return;
+        addTodo(text);
+        setText("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <button>Add</button>
+        </form>
+    )
 }
 
 export default TodoForm
